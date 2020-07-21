aa3d409b86efa12c9b709b0a6e6862525e34ca71
Lorenzo Mateo <kingkile214@gmail.com>
2020-07-22 07:35:06 +0800

    Added all of the project's files

---
+++ folder_name2/function3.js
@@ -0,0 +1,8 @@
+function my_function(data, result){
+    str = "f3 is done!";
+    result = data.concat(str);
+
+    return result;
+}
+
+module.exports.my_function = my_function;
---
+++ main.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const readdir = util.promisify(fs.readdir);
+
+
+function findFile(filePath,ext){   // function to recursively look for files in a path with a given extension
+    try {
+        // check if path exists
+        if (!fs.existsSync(filePath)){ 
+            console.log("no folder found",filePath);
+            return;
+        }
+        
+        // read the files in the directory specified by the path
+        var files = fs.readdirSync(filePath);
+        resArr = [];
+
+        // loop to get all the files in the directory
+        for (i=0;i<files.length;i++){
+            filename = path.join(filePath,files[i]);
+            stat = fs.lstatSync(filename);
+            if (stat.isDirectory()){
+                findFile(filename,ext);
+            }
+            else if (filename.indexOf(ext)>=0) {  
+                file = "./".concat(filename);         
+                func = require(file);                  
+                result = func.my_function("-- ", "");   // execute function in the current js file in the loop
+                resArr.push(result)                     // store result in an array
+
+            };
+        };
+
+        return resArr;
+
+      } catch (error) {
+        console.log('Error:', error.message);
+      }
+};
+
+// async/await
+var run = async () => {
+    const [folder1, folder2, folder3] = 
+    await Promise.all([
+        findFile('./folder_name1','.js'),
+        findFile('./folder_name2','.js'),
+        findFile('./folder_name3','.js'),
+        
+    ])
+    console.log("Folder 1: " + folder1.toString()); // display contents of folder 1
+    console.log("Folder 2: " + folder2.toString()); // display contents of folder 2
+    console.log("Folder 3: " + folder3.toString()); // display contents of folder 3
+};
+
+run();
